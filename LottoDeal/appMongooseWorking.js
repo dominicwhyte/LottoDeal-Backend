145bcf7a37cd051bbe74f9f9f3b0819238f13de6
Antony Toron <atoron@princeton.edu>
2017-04-06 07:06:03 -0400

    add basic server skeleton

--- LottoDeal/appMongooseWorking.js
+++ LottoDeal/appMongooseWorking.js
@@ -11,16 +11,75 @@ var mongoose = require('mongoose');
 // Connection URL
 var url = 'mongodb://localhost:27017/LottoDeal';
 
+
+/* SERVER FUNCTIONALITY */
+var express = require('express')
+var app = express()
+
+app.get('/', function(request, response) {
+  response.send("API is working!")
+})
+
+// will create a new post, and associate it with a user in our database
+app.post('/createPost', function(request, response) {
+  // Parse the response
+  // response usually comes in response.params or response.query
+
+  // Make sure that the response is in the proper format to prevent malicious access
+
+  console.log(response) // check what is inside response
+  response.send("You have created a new post.")
+})
+
+// Will add a new user to our database
+app.post('/createUser', function(request, response) {
+  response.send("You have created a new user")
+})
+
+// A user has bid on an item, add this bid to database
+app.post('/addBid', function(request, response) {
+  // get into database, access object, update it's bid field and add to user bids
+
+
+  response.send("Bid added")
+})
+
+// Send back all posts
+app.get('/getPosts', function(request, response) {
+  // get all of the posts and return them to frontend to load on feed
+  // might not need to include bids
+
+
+  response.send("Here are all of the posts")
+})
+
+// Send back either a serialized or full version of all users
+app.get('/getUsers', function(request, response) {
+  response.send("Here are all of the users")
+})
+
+// Send back the bids on the passed in item parameter, in case user wants to
+// see the people that bid on his item
+app.get('/getBids', function(request, response) {
+  response.send("Here are all of the bids on this item")
+})
+
+// Start the server at localhost:8000
+app.listen(8000, function() {
+  console.log("App is listening on port 8000")
+})
+
+
+
+/* MONGOOSE FUNCTIONALITY */
+
+
 // Use connect method to connect to the server
-
-
 mongoose.Promise = global.Promise;
 
-
+// Connect to MongoDB database using mongoose API
 mongoose.connect(url, function(err, db) {
     assert.equal(null, err);
-
-
 
   	var newItem = new Item ({title : "bike", price : 100, datePosted : new Date(), expirationDate: new Date(), pictureURL: "google", descrip: "hello"});
 	// call the built-in save method to save to the database
@@ -29,9 +88,6 @@ mongoose.connect(url, function(err, db) {
 
   	console.log('Item saved successfully!');
 	});
-
-
-
     console.log("Connected successfully to server");
 
 });
