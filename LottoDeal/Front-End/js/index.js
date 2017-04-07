5dad15355fe55f3623588b6c836a6ba175047147
Lucas Irvine <lucasirvine@nat-oitwireless-inside-vapornet100-c-7561.Princeton.EDU>
2017-04-07 17:39:53 -0400

    dynamic tabs

--- LottoDeal/Front-End/js/index.js
+++ LottoDeal/Front-End/js/index.js
@@ -3,42 +3,41 @@ var app = angular.module("index_app", [])
 app.controller("indexController", function($scope) {
 	$scope.selectedTab = 0
 
-	/* Create Tabs */
-    $(document).ready(function(){
-        $("#login").click(function(){
-            $(".content").load("./login.html .content");
-            console.log("here");
-            return false;
-        });
-    });
-
-    $(document).ready(function(){
-        $("#about").click(function(){
-            $(".content").load("./about.html .content");
-            return false;
-        });
-    });
-
-    $(document).ready(function(){
-        $("#sell").click(function(){
-            $(".content").load("./sell.html .content");
-            return false;
-        });
-    });
-
-    $(document).ready(function(){
-        $("#contact").click(function(){
-            $(".content").load("./contact.html .content");
-            return false;
-        });
-    });
-
-    $(document).ready(function(){
-        $("#home").click(function(){
-            $(".content").load("./index.html .content");
-            return false;
-        });
-    });
-
 })
 
+/* Create Tabs */
+$(document).ready(function(){
+    $("#login").click(function(){
+        $(".content").load("./login.html .content");
+        console.log("here");
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    $("#about").click(function(){
+        $(".content").load("./about.html .content");
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    $("#sell").click(function(){
+        $(".content").load("./sell.html .content");
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    $("#contact").click(function(){
+        $(".content").load("./contact.html .content");
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    $("#home").click(function(){
+        $(".content").load("./index.html .content");
+        return false;
+    });
+});
