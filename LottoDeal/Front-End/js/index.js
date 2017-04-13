5e177481472c51b223d4dc57df8ba84837bfdb53
Lucas Irvine <lucasirvine@nat-oitwireless-inside-vapornet100-c-7561.Princeton.EDU>
2017-04-13 17:01:03 -0400

    front end changes

--- LottoDeal/Front-End/js/index.js
+++ LottoDeal/Front-End/js/index.js
@@ -5,32 +5,32 @@ app.controller("indexController", function($scope) {
 
 })
 
-/* Create Tabs */
+// /* Create Tabs */
 
-$(document).ready(function(){
-    $("#login").click(function(){
-        $(".content").load("./login.html .content");
-        console.log("here");
-        return false;
-    });
+// $(document).ready(function(){
+//     $("#login").click(function(){
+//         $(".content").load("./login.html .content");
+//         console.log("here");
+//         return false;
+//     });
 
-    $("#about").click(function(){
-        $(".content").load("./about.html .content");
-        return false;
-    });
+//     $("#about").click(function(){
+//         $(".content").load("./about.html .content");
+//         return false;
+//     });
 
-    $("#sell").click(function(){
-        $(".content").load("./sell.html .content");
-        return false;
-    });
+//     $("#sell").click(function(){
+//         $(".content").load("./sell.html .content");
+//         return false;
+//     });
 
-    $("#contact").click(function(){
-        $(".content").load("./contact.html .content");
-        return false;
-    });
+//     $("#contact").click(function(){
+//         $(".content").load("./contact.html .content");
+//         return false;
+//     });
 
-    $("#home").click(function(){
-        $(".content").load("./index.html .content");
-        return false;
-    });
-});
+//     $("#home").click(function(){
+//         $(".content").load("./index.html .content");
+//         return false;
+//     });
+// });
--- LottoDeal/Front-End/js/login.js
+++ LottoDeal/Front-End/js/login.js
@@ -1,19 +1,32 @@
-var app = angular.module("login_app", []); // put any module dependencies here ["ngRoute"]
 
-console.log('Test');
+// For Facebook login
 
-
-
-// app.config(function($routeProvider)) { can put routing here}
+// Facebook Javascript SDK configuration and setup
 window.fbAsyncInit = function() {
     FB.init({
       appId      : '228917890846081',
       xfbml      : true,
+      cookie     : true,
       version    : 'v2.8'
   });
 
-    //FB.AppEvents.logPageView();
+// Check whether the user already logged in
+
+    // Check whether the user already logged in
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            console.log('Logged in.');
+            $("#fbButton").hide();
+            //display user data
+            getFbUserData();
+        }
+        else {
+            console.log('Not logged in');
+        }
+    });
 };
+
+// Load the Javascript SDK asynchronously
 
 (function(d, s, id){
  var js, fjs = d.getElementsByTagName(s)[0];
@@ -21,10 +34,22 @@ window.fbAsyncInit = function() {
  js = d.createElement(s); js.id = id;
  js.src="https://connect.facebook.net/en_US/all.js";
  fjs.parentNode.insertBefore(js, fjs);
-
 }(document, 'script', 'facebook-jssdk'));
 
-function myFacebookLogin() {
+
+// Facebook login with JavaScript SDK
+function fbLogin() {
+    FB.login(function (response) {
+        if (response.authResponse) {
+            // Get and display the user profile data
+            getFbUserData();
+        } else {
+            document.getElementById('status').innerHTML = 'User cancelled login or did not fully authorize.';
+        }
+    }, {scope: 'email'});
+}
+
+function saveUserData(response) {
 
   console.log("got here bro")
 
@@ -35,9 +60,9 @@ function myFacebookLogin() {
       var profileurl = "www.github.com"
 
       data = {
-        name: username,
+        name: response.first_name+ ' ' + response.last_name,
         fbid: userFbid,
-        url: profileurl
+        url: response.link
       }
 
       // AJAX POST TO SERVER
@@ -53,31 +78,40 @@ function myFacebookLogin() {
         console.log(error)
         }
     });
+}
 
-    console.log('test3')
-  // FB.login(function(){}, {scope: 'publish_actions'});
+// Fetch the user profile data from facebook
+function getFbUserData(){
+    FB.api('/me', {locale: 'en_US', fields: 'id,first_name,last_name,email,link,gender,locale,picture'},
+        function (response) {
+            document.getElementById('fbLink').setAttribute("onclick","fbLogout()");
+            document.getElementById('fbLink').innerHTML = 'Logout from Facebook';
+            document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.first_name + '!';
+            document.getElementById('userData').innerHTML = '<p><b>FB ID:</b> '+response.id+'</p><p><b>Name:</b> '+response.first_name+' '+response.last_name+'</p><p><b>Email:</b> '+response.email+'</p><p><b>Gender:</b> '+response.gender+'</p><p><b>Locale:</b> '+response.locale+'</p><p><b>Picture:</b> <img src="'+response.picture.data.url+'"/></p><p><b>FB Profile:</b> <a target="_blank" href="'+response.link+'">click to view profile</a></p>';
+
+            // Save user data
+            saveUserData(response);
+        });
+}
+
+// Logout from facebook
+function fbLogout() {
+    FB.logout(function() {
+        document.getElementById('fbLink').setAttribute("onclick","fbLogin()");
+        document.getElementById('fbLink').innerHTML = '<img src="fblogin.png"/>';
+        document.getElementById('userData').innerHTML = '';
+        document.getElementById('status').innerHTML = 'You have successfully logout from Facebook.';
+    });
 }
 
 
-console.log('test2')
+var app = angular.module("app", []);
 
 app.controller("loginController", function($scope) {
 	$scope.selectedTab = 0;
 
 	// possible jquery tab selection here
 	
-})
-
-var app = angular.module("app", []);
+});
 
 
- //   FB.getLoginStatus(function(response) {
- //      if (response.status === 'connected') {
- //        console.log('Logged in.');
- //        $("#fbButton").hide();
- //    }
- //    else {
- //     console.log('Not logged in')
- //     FB.login();
- // }}
- // );
