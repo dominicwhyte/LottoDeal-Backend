14f2d4122d7001e49ede920e9ea2f82709aef79b
Lucas Irvine <lucasirvine@nat-oitwireless-inside-vapornet100-c-7561.Princeton.EDU>
2017-04-13 03:33:23 -0400

    updates to email

--- LottoDeal/Front-End/js/index.js
+++ LottoDeal/Front-End/js/index.js
@@ -6,36 +6,29 @@ app.controller("indexController", function($scope) {
 })
 
 /* Create Tabs */
+
 $(document).ready(function(){
     $("#login").click(function(){
         $(".content").load("./login.html .content");
         console.log("here");
         return false;
     });
-});
 
-$(document).ready(function(){
     $("#about").click(function(){
         $(".content").load("./about.html .content");
         return false;
     });
-});
 
-$(document).ready(function(){
     $("#sell").click(function(){
         $(".content").load("./sell.html .content");
         return false;
     });
-});
 
-$(document).ready(function(){
     $("#contact").click(function(){
         $(".content").load("./contact.html .content");
         return false;
     });
-});
 
-$(document).ready(function(){
     $("#home").click(function(){
         $(".content").load("./index.html .content");
         return false;
