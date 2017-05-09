123d659d27eb9265cbf3409a0efb48ff7f55e8bc
Dominic Whyte <dwhyte@princeton.edu>
2017-05-08 22:09:55 -0400

    testing timing

--- LottoDeal/server.js
+++ LottoDeal/server.js
@@ -168,12 +168,13 @@ app.post('/performPaymentAndAddBid', function(request, response) {
 //START USER AUTHENTICATION CODE
 
 var httprequest = require("request");
+var performance = require("performance");
 
 //Validates an access token. Provide response and request in case a 404 
 //error should be thrown
 function validateAccessToken(accessToken, response, request, callback) {
     console.log('Validating access Token');
-    //var t0 = performance.now();
+    var t0 = performance.now();
     httprequest({
         uri: "https://graph.facebook.com/me?access_token=" + accessToken,
         method: "GET",
@@ -181,8 +182,8 @@ function validateAccessToken(accessToken, response, request, callback) {
         followRedirect: true,
         maxRedirects: 10
     }, function(error, validateResponse, body) {
-       // var t1 = performance.now();
-        //console.log("Call validateAccessToken took " + (t1 - t0) + " milliseconds.");
+        var t1 = performance.now();
+        console.log("Call validateAccessToken took " + (t1 - t0) + " milliseconds.");
         body = JSON.parse(body);
         var userID = body.id;
 
