8e15a7c014eca77e438e6d33ae60942cc37e2868
Dominic Whyte <dwhyte@princeton.edu>
2017-05-13 01:01:53 -0400

    Bug fixes

--- LottoDeal/server.js
+++ LottoDeal/server.js
@@ -279,7 +279,6 @@ app.get('/getReviewsOfSeller', function(request, response) {
 // Check if a user is already registered in the database
 app.get('/checkIfUser', function(request, response) {
     var accessToken = request.query["accessToken"];
-    console.log('checkifuser running');
     validateAccessToken(accessToken, response, request, function(userID) {
         if (userID != undefined) {
             findUser(userID, function(user) {
@@ -300,6 +299,7 @@ app.get('/checkIfUser', function(request, response) {
 // mark all notifications read
 app.get('/markRead', function(request, response) {
     var accessToken = request.query["accessToken"];
+    console.log('Markread running');
     validateAccessToken(accessToken, response, request, function(userID) {
         if (userID != undefined) {
             User.find({
@@ -327,6 +327,7 @@ app.get('/markRead', function(request, response) {
 //Get account, where account is the private account with all info of the logged in user
 app.get('/getAccount', function(request, response) {
     var accessToken = request.query["accessToken"];
+    console.log('accessToken running');
     validateAccessToken(accessToken, response, request, function(userID) {
         findUser(userID, function(user) {
             if (user != null) {
@@ -402,6 +403,7 @@ function send404(response, request) {
 // send back all the notifications
 app.get('/getNotifications', function(request, response) {
     var accessToken = request.query["accessToken"];
+    console.log('notifications running');
     validateAccessToken(accessToken, response, request, function(userID) {
         getNotificationsForUsers(userID, function(notifications) {
             if (notifications != null) {
@@ -417,6 +419,7 @@ app.get('/getNotifications', function(request, response) {
 //Verifies an access token and returns the userID associated to it
 app.get('/verifyAccessToken', function(request, response) {
     var accessToken = request.query["accessToken"];
+    console.log('verifyAccessToken running');
     validateAccessToken(accessToken, response, request, function(userID) {
         if (userID != null) {
             response.send(userID);
